146ef190e4bc2cb1f0802b6477a88819b1e8b5c2
James Lyne <jim+github@not-null.co.uk>
2025-01-11 23:12:54 +0000

    Refactor gradle scripts
    - Switch to Kotlin
    - Switch to plugin-yml gradle plugin
    - Switch to using version catalog for dependencies
    - Follow gradle documentation's method for publishing credentials

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    java
+    alias(libs.plugins.pluginYmlPaper)
+}
+
+group = "uk.co.notnull.playertime"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	maven {
+		url = uri("https://repo.papermc.io/repository/maven-public/")
+	}
+}
+
+dependencies {
+	compileOnly(libs.paperApi)
+}
+
+paper {
+    main = "uk.co.notnull.playertime.PlayerTime"
+    apiVersion = libs.versions.paperApiVersion.get().replace(Regex("\\-R\\d.\\d-SNAPSHOT"), "")
+    authors = listOf("Jim (AnEnragedPigeon)")
+    description = "Per player time"
+
+    permissions {
+        register("ptime.command") {
+            description = "Allows use of /ptime"
+            default = BukkitPluginDescription.Permission.Default.TRUE
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        dependsOn(clean)
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+        options.encoding = "UTF-8"
+    }
+}
