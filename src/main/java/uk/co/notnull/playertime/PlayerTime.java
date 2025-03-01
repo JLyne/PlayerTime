1ef3ebb42af922c2a73899730ab5ffe6f114c024
James Lyne <jim+github@not-null.co.uk>
2025-03-01 16:58:11 +0000

    Update gradle

--- src/main/java/uk/co/notnull/playertime/PlayerTime.java
+++ src/main/java/uk/co/notnull/playertime/PlayerTime.java
@@ -5,11 +5,12 @@ import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("UnstableApiUsage")
 public class PlayerTime extends JavaPlugin implements Listener {
 	public void onEnable() {
-		LifecycleEventManager<Plugin> manager = getLifecycleManager();
+		LifecycleEventManager<@NotNull Plugin> manager = getLifecycleManager();
         manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> new CommandHandler(this, event.registrar()));
    }
 }
